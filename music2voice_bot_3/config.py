f1119c27f413b30d08bad795588c3077cb37be74
MazeWindow <windowmaze43@gmail.com>
2020-12-18 19:07:15 +0200

    redis attached

--- music2voice_bot_3/config.py
+++
@@ -1,5 +0,0 @@
-TOKEN="1452244142:AAGQM4OEbX8D2bm_utrbdpMvFXfrUijC9WU"
-SERVER_FILES_DIR="https://api.telegram.org/file/bot"+TOKEN+"/"
-LOCAL_MP3_DIR="music/"
-LOCAL_OPUS_DIR="opus/"
-TIMING_DELIMITERS_ROAM=["-", ",", ";", " "]
